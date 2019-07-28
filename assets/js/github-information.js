f2aec6665f82c81d4eba74cd651a3e60ba401c6b
Igor K <igorkk@live.ru>
2019-07-28 12:46:57 +0000

    Added throttling 403 error response to GitHubs API

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -72,6 +72,10 @@ function fetchGitHubInformation(event) {
                 $("gh-user-data").html(
                     `<2>No info found for user ${username}</h2>`);
             }
+            else if(errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset')*1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
+            }
             else {
                 console.log(errorResponse);
                 $("gh-user-data").html(
@@ -79,3 +83,6 @@ function fetchGitHubInformation(event) {
             }
         });
 }
+
+
+$(document).ready(fetchGitHubInformation);
